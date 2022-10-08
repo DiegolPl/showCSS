47e8317e578656969a6d4f4764bfbc005fb5b8e9
DiegolPl <diegopdl1992@gmail.com>
2022-10-08 12:36:43 -0300

    Agrego componentes de botones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './botones/buttons/buttons.component';
+import { HoverComponent } from './botones/hover/hover.component';
+import { MenuComponent } from './botones/menu/menu.component';
+import { ProgressbarComponent } from './botones/progressbar/progressbar.component';
+import { TransitionComponent } from './botones/transition/transition.component';
+import { AnimationComponent } from './botones/animation/animation.component';
+import { OthersComponent } from './botones/others/others.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    HoverComponent,
+    MenuComponent,
+    ProgressbarComponent,
+    TransitionComponent,
+    AnimationComponent,
+    OthersComponent
   ],
   imports: [
     BrowserModule,
